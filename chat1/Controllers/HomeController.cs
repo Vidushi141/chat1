56b60a880d68fe94e9463afb885af6680410d3f2
Vee Bist <vidushibist42@gmail.com>
2023-06-12 13:17:23 +0530

    Add project files.

---
+++ chat1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿// HomeController.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TutorialSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is a tutorial website built with C# and ASP.NET MVC.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact us for any queries.";
+
+            return View();
+        }
+    }
+}
